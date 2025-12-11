be93d8967884291f4ac179703614cbbe15f641d8
nuc130n <cedric.gain@econocom.com>
2025-12-11 11:42:37 +0100

    ASM version

---
+++ ASM-APIClient/ASM_APIClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASM_APIClientApp.swift
+//  ASM-APIClient
+//
+//  © Created by Somesh Pathak on 23/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ASM_APIClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ASM-APIClientTests/ASM_APIClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  ASM_APIClientTests.swift
+//  ASM-APIClientTests
+//
+//  Created by Somesh Pathak on 23/06/2025.
+//
+
+import Testing
+@testable import ASM_APIClient
+
+struct ASM_APIClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
